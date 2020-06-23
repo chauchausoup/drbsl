7d12cef2fd883f7d157e2b59fdac4e1a224fcaca
chauchausoup <7krishna7dhakal7@gmail.com>
2020-06-23 18:58:54 +0545

    3rd challange state to DOM

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 //import About from './components/About';
 //import user  from './data/user';
-import UserLoop from './components/UserLoop';
+//import UserLoop from './components/UserLoop';
+import Update from './components/Update';
 
 
 function App() {
   return (
     <div >
-      <UserLoop/>
+      <Update/>
     </div>
   );
 }
