29e626d451f819fbc591b7004c8ab722b606090b
chauchausoup <7krishna7dhakal7@gmail.com>
2020-07-01 17:20:41 +0545

    start

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import './App.css';
-//import About from './components/About';
-//import user  from './data/user';
-//import UserLoop from './components/UserLoop';
-//import UpdateHooks from './components/UpdateHooks';
-//import OpenSesame from './components/OpenSesame';
-import Home from './components/IceAndFire'
+
 
 
 function App() {
   return (
     <div >
-      <Home />    
+      
     </div>
   );
 }
