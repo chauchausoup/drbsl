a5ce9e7c9a29d6884b8dbafa8d9d8a51d03f6ca1
chauchausoup <7krishna7dhakal7@gmail.com>
2020-07-01 22:07:38 +0545

    before using react router nav is tentative

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/NavigationBar'
+import NavBar from './components/Navbar/NavigationBar'
 
 
 function App() {
---
+++ src/components/Navbar/NavigationBar.js
@@ -0,0 +1,47 @@
+import React,{useState} from 'react';
+import '/home/krishnadhakal/Desktop/drbsl/src/styles/Navbar.css';
+
+export default function NavigationBar(){
+
+          const[isOpen,setIsOpen]=useState(false);
+
+          function handleClick(){
+                    setIsOpen(!isOpen)
+          }
+
+          return(
+                    <div>
+                              <nav className="Nav">
+                                        <div className="head">
+                                                  <h1 id="headline">drbuddhisagarlamichhane.com</h1>
+                                                  <div id="btn" onClick={handleClick}>
+                                                            -
+                                                  </div>
+                                        </div>
+                                        <div className= {isOpen?"drop":"ceil"}> 
+                                                  <ul id="socials1">
+                                                            <li><a href="/#">Email</a></li>
+                                                            <li><a href="/#">Facebook</a></li>
+                                                            <li><a href="/#">Viber</a></li>
+                                                  
+                                                  
+                                                  </ul>
+                                                  <ul id="routes">
+                                                            <li onClick={handleClick}>News</li>
+                                                            <li onClick={handleClick}>Files</li>
+                                                            <li onClick={handleClick}>Login</li>
+                                                  </ul>
+                                                  <ul id="socials2">
+                                                            <li><a href="/#">Email</a></li>
+                                                            <li><a href="/#">Facebook</a></li>
+                                                            <li><a href="/#">Viber</a></li>
+                                                  
+                                                  
+                                                  </ul>
+                                        </div>
+                                        
+                              </nav>
+                    </div>
+          )
+}
+
